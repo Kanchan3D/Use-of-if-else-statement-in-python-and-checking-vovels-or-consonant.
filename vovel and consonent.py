a568c343466304b46271a71767498fb595280007
Kanchan Dasila <118431103+Kanchan3D@users.noreply.github.com>
2023-03-06 18:52:11 +0530

    Add files via upload

---
+++ vovel and consonent.py
@@ -0,0 +1,7 @@
+a=str(input("enter any alphabet"))
+if(a=="A"or a=="E" or a=="I" or a=="O" or a=="U"):
+    print("It is a vovel.")
+elif(a=='a' or a=='e' or a=='i' or a=="o" or a=='u' ):
+    print("It is a vovel.")
+else:
+    print("it is a consonant.")
